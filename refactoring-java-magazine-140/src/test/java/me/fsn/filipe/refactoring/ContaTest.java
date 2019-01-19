32c56d6957c2d044d59d532f5eb5e7ebac3f8030
Elízio Israel <elizioisrael@gmail.com>
2019-01-19 00:01:35 -0200

    Implementando testes antes de começar a refatoração.

--- refactoring-java-magazine-140/src/test/java/me/fsn/filipe/refactoring/ContaTest.java
+++ refactoring-java-magazine-140/src/test/java/me/fsn/filipe/refactoring/ContaTest.java
@@ -8,42 +8,66 @@ public class ContaTest {
 
     @Test
     public void deve_saber_calcular_a_conta_de_um_carro_de_passeio_ate_meia_hora() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-7399", "Fox", TipoVeiculo.CARRO_PASSEIO);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 08:30 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547886600000L, veiculo);
+    	assertEquals(5,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_um_carro_de_passeio_ate_uma_hora() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-7399", "Fox", TipoVeiculo.CARRO_PASSEIO);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 09:00 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547888400000L, veiculo);
+    	assertEquals(8,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_um_carro_de_passeio_ate_com_hora_adicional() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-7399", "Fox", TipoVeiculo.CARRO_PASSEIO);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 14:59 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547909940000L, veiculo);
+    	assertEquals(26,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_um_carro_de_passeio_de_diaria() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-7399", "Fox", TipoVeiculo.CARRO_PASSEIO);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 15:00 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547910000000L, veiculo);
+    	assertEquals(20,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_uma_caminhonete_ate_meia_hora() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-1133", "Amoroc", TipoVeiculo.CAMINHONETE);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 08:30 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547886600000L, veiculo);
+    	assertEquals(7,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_uma_caminhonete_ate_uma_hora() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-1133", "Amoroc", TipoVeiculo.CAMINHONETE);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 09:00 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547888400000L, veiculo);
+    	assertEquals(10,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_uma_caminhonete_ate_com_hora_adicional() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-1133", "Amoroc", TipoVeiculo.CAMINHONETE);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 14:59 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547909940000L, veiculo);
+    	assertEquals(40,0, conta.gerarConta());
     }
 
     @Test
     public void deve_saber_calcular_a_conta_de_uma_caminhonete_de_diaria() {
-        fail("Not yet implemented");
+    	Veiculo veiculo = Veiculo.of("TES-1133", "Amoroc", TipoVeiculo.CAMINHONETE);
+    	// Entrada: 19-01-2019 08:00 (1547884800000L) | Saída: 19-01-2019 15:00 (1547886000000L)
+    	Conta conta = new Conta(1547884800000L, 1547910000000L, veiculo);
+    	assertEquals(30,0, conta.gerarConta());
     }
 
 }
